b9f0cdd1da8ad0fcc8f1f2750a47c32f259e2626
Akash Kennedy <akashkennedy1@gmail.com>
2024-05-26 10:22:48 +0530

    Create Bank Account Header Chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Akash" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and Manage Your Account Transactions Efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1230.89}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
